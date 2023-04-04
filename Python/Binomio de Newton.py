159d15b9e80b84ab302f27b58286d352ff79da5a
Nivaniz <Nivaniz2019@gmail.com>
2023-04-04 14:53:37 -0600

    Actualización y borrar códigos

--- Python/Binomio de Newton.py
+++
@@ -1,4 +0,0 @@
-x = int (input("Favor de ingresar el valor de x: "))
-y = int (input("Favor de ingresar el valor de y: "))
-n = int (input("Favor de ingresar el valor de n: "))
-print ("("+str(x)+"+"+str(y)+")"+str(n)+"=" +str( (x+y)**n ))
