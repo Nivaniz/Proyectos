3100ef09dc127323aa5334300b497cb89a0c82a9
Nivaniz <Nivaniz2019@gmail.com>
2023-04-13 17:45:51 -0600

    Documentación de UI

--- Gestor/ui.py
+++ Gestor/ui.py
@@ -7,20 +7,70 @@ import helpers
 
 
 class CenterWidgetMixin:
-
     def center(self):
+        # Actualiza la ventana antes de obtener el ancho y la altura
         self.update()
+
+        # Obtiene el ancho y la altura de la ventana
         w = self.winfo_width()
         h = self.winfo_height()
+
+        # Obtiene el ancho y la altura de la pantalla
         ws = self.winfo_screenwidth()
         hs = self.winfo_screenheight()
+
+        # Calcula las coordenadas para centrar la ventana
         x = int((ws / 2) - (w / 2))
         y = int((hs / 2) - (h / 2))
+
+        # Establece la geometría de la ventana para que aparezca centrada
         self.geometry(f"{w}x{h}+{x}+{y}")
 
 
 class CreateClientWindow(Toplevel, CenterWidgetMixin):
+    """
+    Ventana de creación de clientes.
+
+    Esta clase representa una ventana de Tkinter que permite crear un cliente.
+    Se asegura de que los datos ingresados sean válidos antes de permitir la
+    creación del cliente.
+
+    Attributes
+    ----------
+    crear : tkinter.Button
+        Botón "Crear" que permite crear un nuevo cliente.
+    curp : tkinter.Entry
+        Entrada de texto que permite ingresar la CURP del cliente.
+    nombre : tkinter.Entry
+        Entrada de texto que permite ingresar el nombre del cliente.
+    apellido : tkinter.Entry
+        Entrada de texto que permite ingresar el apellido del cliente.
+    validation_message : tkinter.Label
+        Etiqueta que muestra un mensaje de validación para los campos de entrada.
+    validaciones : list
+        Lista que almacena los valores de validación de los campos de entrada.
+
+    Methods
+    -------
+    build()
+        Construye los widgets de la ventana.
+    validate(event, index)
+        Valida la entrada de texto de los campos de entrada.
+    create_client()
+        Crea un nuevo cliente a partir de los datos ingresados.
+    close()
+        Cierra la ventana.
+
+    """
     def __init__(self, parent):
+        """
+        Inicializa la ventana.
+
+        Parameters
+        ----------
+        parent : tkinter.Tk
+            La ventana principal de la aplicación.
+        """
         super().__init__(parent)
         self.title('Crear cliente')
         self.build()
@@ -30,6 +80,9 @@ class CreateClientWindow(Toplevel, CenterWidgetMixin):
         self.grab_set()
 
     def build(self):
+        """
+        Construye los widgets de la ventana.
+        """
         # Top frame
         frame = Frame(self)
         frame.pack(padx=20, pady=10)
@@ -74,6 +127,10 @@ class CreateClientWindow(Toplevel, CenterWidgetMixin):
         self.apellido = apellido
 
     def create_client(self):
+        """
+            Agrega un nuevo cliente a la lista de clientes y la base de datos.
+            Cierra la ventana después de crear el cliente.
+        """
         self.master.treeview.insert(
             parent='', index='end', iid=self.curp.get(),
             values=(self.curp.get(), self.nombre.get(), self.apellido.get()))
@@ -81,10 +138,18 @@ class CreateClientWindow(Toplevel, CenterWidgetMixin):
         self.close()
 
     def close(self):
+        """
+           Destruye la ventana actual y actualiza el estado de la ventana principal.
+        """
         self.destroy()
         self.update()
 
     def validate(self, event, index):
+        """
+            Valida la entrada del usuario para el widget que desencadenó el evento.
+            Actualiza el mensaje de validación y el color de fondo del widget.
+            Actualiza el estado del botón Crear si todas las validaciones son verdaderas.
+        """
         valor = event.widget.get()
         valido = False
         if index == 0:
@@ -130,13 +195,18 @@ class CreateClientWindow(Toplevel, CenterWidgetMixin):
         # Cambiar estado del botón con base en las validaciones
         self.validaciones[index] = valido
         self.crear.config(state=NORMAL if self.validaciones == [1, 1, 1] else DISABLED)
-    # AEMM666666MAS89
-
-# LA DOSSSS
 
 
 class EditClientWindow(Toplevel, CenterWidgetMixin):
+    """
+    Clase que representa la ventana de edición de clientes.
+    """
+
     def __init__(self, parent):
+        """
+        Constructor de la clase.
+        :param parent: Ventana padre.
+        """
         super().__init__(parent)
         self.title('Modificar cliente')
         self.build()
@@ -146,6 +216,9 @@ class EditClientWindow(Toplevel, CenterWidgetMixin):
         self.grab_set()
 
     def build(self):
+        """
+           Método que construye la interfaz de la ventana.
+        """
         # Top frame
         frame = Frame(self)
         frame.pack(padx=20, pady=10)
@@ -196,6 +269,9 @@ class EditClientWindow(Toplevel, CenterWidgetMixin):
         self.apellido = apellido
 
     def update_client(self):
+        """
+        Método que actualiza un cliente en la base de datos y en la tabla de clientes de la ventana principal.
+        """
         cliente = self.master.treeview.focus()
         # Sobreescribimos los datos de la fila seleccionada
         self.master.treeview.item(
@@ -204,10 +280,16 @@ class EditClientWindow(Toplevel, CenterWidgetMixin):
         self.close()
 
     def close(self):
+        """
+        Método que cierra la ventana.
+        """
         self.destroy()
         self.update()
 
     def validate(self, event, index):
+        """
+            Método que valida los datos ingresados en los campos de entrada.
+        """
         valor = event.widget.get()
         valido = False
         if index == 0:
@@ -247,13 +329,33 @@ class EditClientWindow(Toplevel, CenterWidgetMixin):
 
 
 class MainWindow(Tk, CenterWidgetMixin):
+    """
+    La clase MainWindow define la ventana principal de la aplicación.
+
+    Esta ventana contiene un Treeview que muestra la lista de clientes almacenados en la base de datos.
+    También tiene botones para crear, modificar y borrar clientes.
+
+    Atributos:
+        treeview (ttk.Treeview): El widget Treeview que muestra la lista de clientes.
+    """
+
     def __init__(self):
+        """
+        Constructor de la clase MainWindow.
+        Configura la ventana, construye su interfaz de usuario y la centra en la pantalla.
+        """
         super().__init__()
         self.title('Gestor de clientes')
         self.build()
         self.center()
 
     def build(self):
+        """
+        Construye la interfaz de usuario de la ventana principal.
+
+        La interfaz contiene un Treeview que muestra la lista de clientes almacenados en la base de datos,
+        así como botones para crear, modificar y borrar clientes.
+        """
         # Top Frame
         frame = Frame(self)
         frame.pack()
@@ -307,6 +409,10 @@ class MainWindow(Tk, CenterWidgetMixin):
         self.treeview = treeview
 
     def delete(self):
+        """
+        Borra el cliente seleccionado en el Treeview y en la base de datos.
+        Pide confirmación antes de borrar al cliente.
+        """
         cliente = self.treeview.focus()
         if cliente:
             campos = self.treeview.item(cliente, 'values')
@@ -320,13 +426,24 @@ class MainWindow(Tk, CenterWidgetMixin):
                 db.Clientes.borrar(campos[0])
 
     def create(self):
+        """
+        Crea una nueva ventana para agregar un nuevo cliente.
+        """
         CreateClientWindow(self)
 
     def edit(self):
+        """
+        Abre una ventana de edición de clientes si se ha seleccionado
+        previamente un cliente en el treeview.
+        """
         if self.treeview.focus():
             EditClientWindow(self)
 
 
 if __name__ == "__main__":
+    """
+    Código principal de la aplicación. Crea una instancia de la ventana
+    principal y la muestra en pantalla.
+    """
     app = MainWindow()
     app.mainloop()
