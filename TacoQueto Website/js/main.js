1290ca8989e9559c21bdeeca33953f5f061e8c05
Nivaniz <Nivaniz2019@gmail.com>
2023-04-13 18:11:21 -0600

    Documentación Página Web

--- TacoQueto Website/js/main.js
+++ TacoQueto Website/js/main.js
@@ -1,31 +1,61 @@
+/**
+ * Guarda la posición actual del scroll
+ * @type {number}
+ */
 let ubicacionPrincipal = window.pageYOffset; //0
 
-  AOS.init();
+// Inicializa AOS
+AOS.init();
 
+/**
+ * Escucha el evento scroll y oculta o muestra la barra de navegación dependiendo de la dirección del scroll
+ */
 window.addEventListener("scroll", function(){
+    /**
+     * Guarda la posición actual del scroll
+     * @type {number}
+     */
     let desplazamientoActual = window.pageYOffset; //180
+
     if(ubicacionPrincipal >= desplazamientoActual){ // 200 > 180
+        // Si el scroll es hacia arriba, muestra la barra de navegación
         document.getElementsByTagName("nav")[0].style.top = "0px"
     }else{
+        // Si el scroll es hacia abajo, oculta la barra de navegación
         document.getElementsByTagName("nav")[0].style.top = "-100px"
     }
+
+    // Actualiza la ubicación principal con la posición actual del scroll
     ubicacionPrincipal= desplazamientoActual; //200
+});
 
-})
-
-// Menu
-
+/**
+ * Selecciona los enlaces de la barra de navegación y el botón de hamburguesa
+ * @type {NodeList}
+ */
 let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+let hamburguer = document.querySelectorAll(".hamburguer")[0];
+
+/**
+ * Variable booleana para controlar el estado del menú
+ * @type {boolean}
+ */
 let semaforo = true;
 
-document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
+/**
+ * Escucha el evento click del botón de hamburguesa y muestra u oculta el menú
+ */
+hamburguer.addEventListener("click", function(){
     if(semaforo){
+        // Si el menú está oculto, cambia el color del botón de hamburguesa a blanco
         document.querySelectorAll(".hamburguer")[0].style.color ="#fff";
         semaforo= false;
     }else{
+        // Si el menú está visible, cambia el color del botón de hamburguesa a negro
         document.querySelectorAll(".hamburguer")[0].style.color ="#000";
         semaforo= true;
     }
 
+    // Muestra u oculta el menú
     enlacesHeader.classList.toggle("menudos")
-})
+});
--- TacoQueto Website/js/menu.js
+++ TacoQueto Website/js/menu.js
@@ -1,11 +1,17 @@
+/**
+ * Selecciona el botón hamburguesa y el menú de navegación y añade un listener para mostrar/ocultar el menú.
+ */
 const hamburguer = document.querySelector('.hamburguer')
 const menu = document.querySelector('.menu-navegacion')
-
 
 hamburguer.addEventListener('click', ()=>{
     menu.classList.toggle("spread")
 })
 
+/**
+ * Añade un listener en la ventana para cerrar el menú si se hace clic fuera del menú o del botón hamburguesa.
+ * @param {Event} e - El evento de clic en la ventana.
+ */
 window.addEventListener('click', e =>{
     if(menu.classList.contains('spread') 
         && e.target != menu && e.target != hamburguer){
@@ -13,4 +19,3 @@ window.addEventListener('click', e =>{
         menu.classList.toggle("spread")
     }
 })
-
